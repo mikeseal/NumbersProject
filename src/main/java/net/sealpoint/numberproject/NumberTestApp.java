7e5d4a1224ba1f1f962a244c7472b50e0267beed
mike.seal@gmail.com <mike@sealpoint.net>
2021-07-30 22:39:16 +0100

    Feature 1 update

--- src/main/java/net/sealpoint/numberproject/NumberTestApp.java
+++ src/main/java/net/sealpoint/numberproject/NumberTestApp.java
@@ -21,7 +21,7 @@ public class NumberTestApp {
         TestNumber = numGen.GenerateNumber();
         System.out.println(TestNumber);
         System.out.println("Added some more functionality");
-        System.out.println("Added for Feature 1");
+        System.out.println("Added for Feature");
     }
     
 }
